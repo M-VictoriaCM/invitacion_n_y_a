062d73b3d7046e4aa6fcdd97e7177781c73ef1c0
MVictoriaCM <mvittoria7@gmail.com>
2024-09-09 22:11:02 -0300

    integracion de firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzWALVKLEbpATt1SW1xl1O6Lo4vhYA3v0",
+  authDomain: "teinviteok.firebaseapp.com",
+  projectId: "teinviteok",
+  storageBucket: "teinviteok.appspot.com",
+  messagingSenderId: "1080639716874",
+  appId: "1:1080639716874:web:3e4f06e76e0bda346829f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
